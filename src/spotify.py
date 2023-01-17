c99e0995d920edcadf039e824cc3750d208cbd0b
Oyama, Shun | Shun | RMI <shun.a.oyama@rakuten.com>
2023-01-17 14:13:23 +0900

    updated EDA and added function to exsisting scripts

--- src/spotify.py
+++ src/spotify.py
@@ -7,6 +7,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import os
 from pathlib import Path
 import sys
+from sklearn.preprocessing import StandardScaler
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -25,10 +26,7 @@ class Spotify:
 
         self.searched_artist = searched_artist
 
-        self.result_artists_df, self.top_song_df, self.all_albums_df, self.all_songs_df, self.all_song_feature_df = self._get_artist_data_wrapper()
-
-
-
+        self.result_artists_df, self.top_song_df, self.all_albums_df, self.all_song_general_info_df, self.all_song_feature_df = self._get_artist_data_wrapper()
 
 
     def _initialize_spotipy(self):
@@ -168,10 +166,54 @@ class Spotify:
             album_tracks_df_list.append(single_album_tracks_df)
 
         all_songs_df = pd.concat(album_tracks_df_list)
+
+        all_song_general_info_df = self._get_all_songs_general_informations(all_songs_df)
+        all_song_general_info_df = self._remove_duplicated_tracks_wrapper(all_song_general_info_df, mode = 'find_top_popularity_among_tracks')
+
         all_song_feature_df = self._get_all_songs_with_audio_features(all_songs_df)
+        # all_song_feature_df = self._remove_duplicated_tracks_wrapper(all_song_feature_df, mode = 'remove_outlier')
 
 
-        return all_songs_df, all_song_feature_df
+        return all_song_general_info_df, all_song_feature_df
+
+    def _get_all_songs_general_informations(self, all_songs_df):
+        all_song_general_info_df_list = []
+
+        limit_at_once = 50
+        for n in range(1, int(len(all_songs_df)/limit_at_once)+2):
+            if n!=1:
+                start_index = limit_at_once*(n-1)
+                end_index = limit_at_once*n
+            else:
+                start_index = 0
+                end_index = limit_at_once
+            
+            song_info_result = self.spotify.tracks(list(all_songs_df['id'][start_index:end_index]))
+            
+            track_info_list = []
+            for idx, track in enumerate(song_info_result['tracks']):
+                track_info = {'name': track['name'],
+                            'popularity': track['popularity'],
+                            'artists': track['artists'][0]['external_urls']['spotify'],
+                            'available_markets': track['available_markets'],
+                            'disc_number': track['disc_number'],
+                            'explicit': track['explicit'],
+                            'external_ids': track['external_ids']['isrc'],
+                            'href': track['href'],
+                            'id': track['id'],
+                            'is_local': track['is_local'],
+                            'preview_url': track['preview_url'],
+                            'track_number': track['track_number'],
+                            'type': track['type'],
+                            'uri': track['uri']
+                            }
+                track_info_list.append(track_info)
+            track_info_df = pd.DataFrame(track_info_list)
+            all_song_general_info_df_list.append(track_info_df)
+            
+        all_song_general_info_df = pd.concat(all_song_general_info_df_list)
+
+        return all_song_general_info_df
 
     def _get_all_songs_with_audio_features(self, all_songs_df):
         '''
@@ -197,6 +239,81 @@ class Spotify:
         all_song_feature_df = pd.concat(all_song_feature_df_list)
 
         return all_song_feature_df
+
+    def _check_outlier(self, row, threshold):
+        n_outliers = len([value for value in row.values if abs(value)>1 ])
+
+        if n_outliers > threshold:
+            return False
+        else:
+            return True
+
+    def _standard_scaling(self, data):
+        scaler = StandardScaler()
+        scaler.fit(data)
+        scaled_df = pd.DataFrame(scaler.transform(data))
+
+        return scaled_df
+
+
+    def _find_top_popularity_among_same_tracks(self, duplicated_tracks_df):
+        duplicated_tracks_df = duplicated_tracks_df.copy()
+        top_popularity_df = duplicated_tracks_df[duplicated_tracks_df['popularity']==duplicated_tracks_df['popularity'].max()].reset_index(drop=True).iloc[0]
+        
+        return top_popularity_df
+
+    def _remove_outlier(self, track_df):
+        track_df = track_df.copy()
+
+        # Drop unnecessary categorical columns 
+        track_df = track_df.drop(['type', 'id', 'uri', 'track_href', 'analysis_url'], axis=1)       
+
+        track_df = track_df.reset_index(drop = True)
+        # Scaling values and see value distribution among those duplicated tracks.
+        data = track_df[track_df.columns[1:]]
+        print(data)
+        scaled_df = self._standard_scaling(data)
+        scaled_df.columns = track_df.columns[1:]
+
+        # Define Threshold 
+        threshold = len(scaled_df.columns)/2
+
+        # Get index of non-outlier tracks
+        non_outlier_index = scaled_df.apply(lambda x: self._check_outlier(x, threshold), axis=1)
+
+        cleaned_track_df = track_df[non_outlier_index]
+        cleaned_track_df = cleaned_track_df.groupby(['name']).mean().reset_index()
+
+        return cleaned_track_df
+
+
+    def _remove_duplicated_tracks_wrapper(self, songs_df, mode = 'remove_outlier'):
+        songs_df = songs_df.copy()
+
+        # Get duplicated tracks and counts
+        track_count_df = songs_df['name'].value_counts().to_frame().reset_index()
+        track_count_df_duplicates = track_count_df[track_count_df['name']>1]
+        duplicated_tracks = list(track_count_df_duplicates['index'].unique())
+
+
+        # Separate original df into non_duplicated_tracks and duplicated_tracks df
+        not_duplcated_tracks_df = songs_df[~songs_df['name'].isin(duplicated_tracks)]
+
+        duplicated_tracks_df = songs_df[songs_df['name'].isin(duplicated_tracks)]
+
+        if mode == 'find_top_popularity_among_tracks':
+            # For duplicated track, it will remove outlier 
+            removed_duplicated_tracks_df= duplicated_tracks_df.groupby(['name']).apply(lambda x:self._find_top_popularity_among_same_tracks(x))
+
+        elif mode == 'remove_outlier':
+            # For duplicated track, it will remove outlier 
+            removed_duplicated_tracks_df = duplicated_tracks_df.groupby(['name']).apply(lambda x:self._remove_outlier(x))
+
+        # Re-merge two dataframe to make it original 
+        cleaned_tracks_df = pd.concat([not_duplcated_tracks_df, removed_duplicated_tracks_df]).reset_index(drop = True)
+
+        return cleaned_tracks_df
+
 
     
 
